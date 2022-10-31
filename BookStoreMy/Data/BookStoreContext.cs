da14235a106cab0a388991e5ae7b4ce35a574bd7
vasiliv <32122468+vasiliv@users.noreply.github.com>
2022-10-31 18:49:32 +0400

    part 48 finished

---
+++ BookStoreMy/Data/BookStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStoreMy.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BookStoreMy.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options)
+        {
+
+        }
+        public DbSet<BookModel> Books { get; set; }
+    }
+}
